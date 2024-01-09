8bd1c29abc627e1c719db8941600c8e94ade0be3
Rodrigo Andrade <rodrigormda@hotmail.com>
2024-01-08 21:01:06 -0300

    docs

--- motion_minder/printer_odometer.py
+++ motion_minder/printer_odometer.py
@@ -13,7 +13,15 @@ _NAMESPACE = "motion_minder"
 
 
 class PrinterOdometer:
-    def __init__(self, update_interval=20):
+    """
+    This class is responsible for calculating the printer's odometer.
+    """
+
+    def __init__(self, update_interval: int = 20) -> None:
+        """
+
+        :param update_interval: The interval in messages between each odometer update.
+        """
         self._id = random.randint(0, 10000)
 
         x, y, z = motion_minder.get_odometer(f"http://{_MOONRAKER_URL}", _NAMESPACE)
@@ -38,7 +46,12 @@ class PrinterOdometer:
         )
         self.websocket.run_forever(reconnect=5)
 
-    def _get_homed_axis(self):
+    def _get_homed_axis(self) -> None:
+        """
+        Get the homed axis from the printer.
+
+        :return:
+        """
         ret = requests.get(f"http://{_MOONRAKER_URL}/printer/objects/query?toolhead")
         self._homed_axis = ""
         try:
@@ -48,7 +61,13 @@ class PrinterOdometer:
         except:
             pass
 
-    def check_klipper_state_routine(self):
+    def check_klipper_state_routine(self) -> None:
+        """
+        Check the klipper state and subscribe to the websocket when it's ready.
+        Always when the Klipper is offline all the websocket subscriptions are lost.
+
+        :return:
+        """
         while True:
             if not self._subscribed:
                 try:
@@ -62,12 +81,26 @@ class PrinterOdometer:
                     pass
             time.sleep(2)
 
-    def _update_single_axis_odometer(self, axis, value):
+    def _update_single_axis_odometer(self, axis: str, value: float) -> None:
+        """
+        Update the odometer for a single axis.
+
+        :param axis: The axis to update.
+        :param value: The current position of the axis, it will calculate the difference between the last position and
+            the current position and add it to the odometer.
+        :return:
+        """
         if self._last_position[axis] is not None:
             self._odom[axis] += abs(value - self._last_position[axis])
         self._last_position[axis] = value
 
-    def _process_motion_report(self, param):
+    def _process_motion_report(self, param: dict) -> None:
+        """
+        Process the motion report and update the odometer.
+
+        :param param: The message received from the websocket that can contain the motion report or not.
+        :return:
+        """
         if "motion_report" not in param:
             return
 
@@ -88,6 +121,12 @@ class PrinterOdometer:
             )
 
     def _process_toolhead(self, param):
+        """
+        Process the toolhead message and get the homed axes.
+
+        :param param: The message received from the websocket that can contain the toolhead message or not.
+        :return:
+        """
         if not "toolhead" in param:
             return
         homed_axes = param["toolhead"].get("homed_axes", None)
@@ -95,6 +134,12 @@ class PrinterOdometer:
             self._homed_axis = homed_axes
 
     def _process_klipper_state(self, param):
+        """
+        Process the klipper state and subscribe to the websocket when it's ready.
+
+        :param param: The message received from the websocket that can contain the klipper state or not.
+        :return:
+        """
         if not "klipper" in param:
             return
         klipper = param["klipper"]
@@ -102,7 +147,14 @@ class PrinterOdometer:
         if state is not None and state == "inactive":
             self._subscribed = False
 
-    def on_message(self, ws, message):
+    def on_message(self, ws, message) -> None:
+        """
+        Process the message received from the websocket.
+
+        :param ws: The websocket object.
+        :param message: The message received from the websocket.
+        :return:
+        """
         message = json.loads(message)
         params = message["params"]
         for param in params:
@@ -111,6 +163,12 @@ class PrinterOdometer:
             self._process_klipper_state(param)
 
     def subscribe(self, websock):
+        """
+        Subscribe to a specific topic from moonraker.
+
+        :param websock: The websocket object.
+        :return:
+        """
         subscribe_objects = {
             "motion_report": None,
             "toolhead": ["homed_axes"],
@@ -128,6 +186,12 @@ class PrinterOdometer:
         )
 
     def on_open(self, ws):
+        """
+        Subscribe to the websocket when it's open.
+
+        :param ws: The websocket object.
+        :return:
+        """
         self.subscribe(ws)
 
 
